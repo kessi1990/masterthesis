e7a2fbfaad3f803e132b894bace5614e51a9e0d3
Michael Kessler <kesslermi@cip.ifi.lmu.de>
2020-08-20 03:15:10 +0200

    - fix sbatch_all_c.sh

--- sh/sbatch_all_c.sh
+++ sh/sbatch_all_c.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 for i in `seq 0 29`;
     do
-        sbatch --partition=All --gres=gpu:0 --exclusive run.sh $i
+        sbatch --partition=All --gres=gpu:0 --exclusive run_c.sh $i
     done
