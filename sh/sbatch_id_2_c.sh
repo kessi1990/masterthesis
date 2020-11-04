1ff735e1c9a1152a6dc991b80e2c1ea8d3b394de
kesslermi <kesslermi@cip.ifi.lmu.de>
2020-11-04 17:02:17 +0100

    - shell scripts

--- sh/sbatch_id_2_c.sh
+++ sh/sbatch_id_2_c.sh
@@ -1,52 +1,52 @@
 #!/usr/bin/env bash
 
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 1 2 add 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 1 2 concat 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 1 2 add 128
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 1 2 concat 128
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 1 2 add 64
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 1 2 concat 64
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 1 2 add 128
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 1 2 concat 128
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 2 2 add 64
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 2 2 concat 64
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 2 2 add 128
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Breakout-v0 2 2 concat 128
 
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 1 2 add 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 1 2 concat 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 1 2 add 128
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 1 2 concat 128
-# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 2 2 add 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 1 2 add 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 1 2 concat 64
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 1 4 add 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 1 2 concat 128
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 2 4 add 64
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 2 2 concat 64
-# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 2 2 add 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 2 2 add 128
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Seaquest-v0 2 2 concat 128
 
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 1 2 add 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 1 2 concat 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 1 2 add 128
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 1 2 concat 128
-# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 2 2 add 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 1 2 add 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 1 2 concat 64
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 1 4 add 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 1 2 concat 128
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 2 4 add 64
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 2 2 concat 64
-# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 2 2 add 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 2 2 add 128
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh BeamRider-v0 2 2 concat 128
 
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 1 2 add 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 1 2 concat 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 1 2 add 128
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 1 2 concat 128
-# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 2 2 add 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 1 2 add 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 1 2 concat 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 1 2 add 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 1 2 concat 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 2 2 add 64
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 2 2 concat 64
-# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 2 2 add 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 2 2 add 128
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh SpaceInvaders-v0 2 2 concat 128
 
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 1 2 add 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 1 2 concat 64
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 1 2 add 128
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 1 2 concat 128
-# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 2 2 add 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 1 2 add 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 1 2 concat 64
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 1 2 add 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 1 2 concat 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 2 2 add 64
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 2 2 concat 64
-# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 2 2 add 128
+#sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 2 2 add 128
 # sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_darqn.sh Pong-v0 2 2 concat 128
 
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh Breakout-v0 0 2
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh Seaquest-v0 0 2
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh BeamRider-v0 0 2
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh SpaceInvaders-v0 0 2
-sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh Pong-v0 0 2
+sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh Breakout-v0 0 7
+sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh Seaquest-v0 0 7
+sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh BeamRider-v0 0 7
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh SpaceInvaders-v0 0 5
+# sbatch --partition=Gobi,Luna --gres=gpu:0 --exclusive batch_dqn.sh Pong-v0 0 5
